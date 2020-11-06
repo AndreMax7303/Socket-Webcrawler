419560759ab7750727ef3c291a9fe405eb95da95
Andre Barata <andre.barata@b2wdigital.com>
2020-11-06 19:21:58 -0300

    refactor: now TCPClient is a context manager - remove the need to close the connection as 'with' takes care of it - changes the flow of the program: a method is used to create the connection and request - accepts custom hostname and paths

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 from tcp_client import TCPClient
-tcp_client = TCPClient('www.google.com')
-print(tcp_client.request())
-tcp_client.close()
+from bs4 import BeautifulSoup
+
+
+def split_url(url):
+    url = url.split('/')
+    hostname = url[0]
+    del url[0]
+    path = ''.join(url)
+    return hostname, path
+
+
+def request(url):
+    hostname, path = split_url(url)
+    with TCPClient(hostname) as tcp_client:
+        response = tcp_client.request(path)
+        status_code = int(response[9:12])
+        print(status_code)
+        # print(response)
+
+
+
+# 'www.ic.uff.br/~vefr/'
+request('www.ic.uff.br/~vefr/')
+# soup = BeautifulSoup(tcp_client.request(), 'html.parser')
--- tcp_client.py
+++ tcp_client.py
@@ -1,13 +1,18 @@
 import socket
 
+
 class TCPClient:
-    def __init__(self, address):
+    def __init__(self, hostname):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((address, 80))
+        self.hostname = hostname
 
-    def request(self):
-        self.socket.send('GET / HTTP/1.1\r\n\r\n'.encode('UTF-8'))
-        return self.socket.recv(4096)
+    def __enter__(self):
+        self.socket.connect((self.hostname, 80))
+        return self
 
-    def close(self):
+    def request(self, path):
+        self.socket.send('GET /{}/ HTTP/1.1 \r\nHost: {} \r\n\r\n'.format(path, self.hostname).encode('UTF-8'))
+        return self.socket.recv(4096 * 100).decode('UTF-8', 'replace')
+
+    def __exit__(self, type, value, tb):
         self.socket.close()

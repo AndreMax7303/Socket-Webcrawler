09f57b3810c5fce6495d3902c6bc0cea2974f0d6
Andre Barata <andre.barata@b2wdigital.com>
2020-11-06 21:28:40 -0300

    fix: changes the request to check for content-length

--- main.py
+++ main.py
@@ -1,25 +1,40 @@
 from tcp_client import TCPClient
 from bs4 import BeautifulSoup
+from enum import Enum
+
+class DownloadType(Enum):
+    TXT = 1
+    IMG = 2
 
 
 def split_url(url):
     url = url.split('/')
+    if '.' not in url[len(url)-1]:
+        url[len(url)-1] += '/'
     hostname = url[0]
     del url[0]
-    path = ''.join(url)
+    path = '/'.join(url)
     return hostname, path
 
 
-def request(url):
+def request(url, download_type):
     hostname, path = split_url(url)
     with TCPClient(hostname) as tcp_client:
         response = tcp_client.request(path)
         status_code = int(response[9:12])
+        # if status_code != 200:
+        #     print('Status: {}'.format(status_code))
+        #     if download_type == DownloadType.TXT:
+        #         print('!!!Exiting program!!!')
+        #         return
+        #     else:
+        #         print('URL: {}'.format(url))
+        #         return
         print(status_code)
         print(response)
 
 
 
 # 'www.ic.uff.br/~vefr/'
-request('www.ic.uff.br/~vefr/')
+request('www.ic.uff.br/~vefr', DownloadType.IMG)
 # soup = BeautifulSoup(tcp_client.request(), 'html.parser')
--- tcp_client.py
+++ tcp_client.py
@@ -1,6 +1,15 @@
 import socket
 
 
+def get_msg_length(response):
+    found = next((s for s in response if b'Content-Length' in s), None)
+    if found:
+        length = int(found.split(b' ')[1])
+        return True, length
+    else:
+        return False, 0
+
+
 class TCPClient:
     def __init__(self, hostname):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,14 +21,15 @@ class TCPClient:
         return self
 
     def request(self, path):
-        self.socket.send('GET /{}/ HTTP/1.1 \r\nHost: {} \r\n\r\n'.format(path, self.hostname).encode('UTF-8'))
-        result = ''
-        while True:
-            received = self.socket.recv(self.buffsize).decode('UTF-8', 'replace')
-            result += received
-            if len(received) < self.buffsize:
-                break
-        return result
+        print(path)
+        self.socket.send('GET /{} HTTP/1.1 \r\nHost: {} \r\nAccept-Encoding: identity \r\n\r\n'.format(path, self.hostname).encode('UTF-8'))
+        received = self.socket.recv(self.buffsize)
+        received_split = received.split(b'\r\n\r\n')[0].split(b'\r\n')
+        is_content_length, content_length = get_msg_length(received_split)
+        if is_content_length:
+            while len(received) < content_length:
+                received += self.socket.recv(self.buffsize)
+        return received
 
     def __exit__(self, type, value, tb):
         self.socket.close()
